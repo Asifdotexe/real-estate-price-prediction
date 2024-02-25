375b9b1fb2717a5fa3efc2f1968f5e1166aa7737
Asif Sayyed <asifdotexe@gmail.com>
2024-02-25 17:33:26 +0530

    #5 created a route for predict_home_price

--- server/server.py
+++ server/server.py
@@ -11,6 +11,22 @@ def get_locations():
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
+@app.route('/predict_home_price', methods=['POST'])
+def predict_home_price():
+    total_sqft = float(request.form['total_sqft'])
+    location = request.form['location']
+    bhk = int(request.form['bhk'])
+    bath = int(request.form['bath'])
+
+    response = jsonify({
+        'estimated_price': util.get_estimated_price(location,total_sqft,bhk,bath)
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
+
+
+
 if __name__ == "__main__":
     print("Starting Python Flask server for House Price Prediction...")
     app.run()
